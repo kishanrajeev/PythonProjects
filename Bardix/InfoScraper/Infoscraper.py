77088c01c45bad058383d8c96baa8aa5112acf74
Kishan Rajeev <125786083+kishanrajeev@users.noreply.github.com>
2023-05-03 09:13:00 -0400

    Added Instagram support - not finished

--- Bardix/InfoScraper/Infoscraper.py
+++ Bardix/InfoScraper/Infoscraper.py
@@ -64,9 +64,38 @@ def twitter_followers_scraper():
     print(final_twitter_name, 'has', final_twitter_followers, 'on Twitter')
     driver.quit()
 
+def instagram_followers_scraper():
+    global twitter_followers
+    edge_options = EdgeOptions()
+    edge_options.use_chromium = True
+    edge_options.add_argument("user-data-dir=C:\\Users\\kisha\\AppData\\Local\\Microsoft\\Edge\\ud1")
+    edge_options.add_argument("--disable-dev-shm-usage")
+    #edge_options.add_argument("--headless")  
+
+    driver_path = "C:\\Users\\kisha\\OneDrive\\Kishan All\\Documents\\msedgedriver.exe"
+    driver = Edge(executable_path=driver_path, options=edge_options)
+    driver.get("https://www.instagram.com/")
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div/a/div/div[2]/div/div')))
+    input_button = driver.find_element("xpath", '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div/a/div').click()
+    input_box = driver.find_element("xpath", '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div[1]/div/input')
+    input_box.send_keys(person_name)
+    input_box.send_keys(Keys.RETURN)
+    wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div/div[2]/a/div/div/div/div[2]/div/div/span[1]/span/div/span')))
+    driver.find_element("xpath", '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div/div[2]/a/div/div/div/div[2]/div/div/span[1]/span/div/span').click()
+    time.sleep(1)
+    instagram_person_name = driver.find_element("xpath", '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/div[1]/a/h2')
+    final_instagram_name = instagram_person_name.text
+    time.sleep(1) 
+    instagram_followers = driver.find_element("xpath", '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/ul/li[2]/a')
+    final_instagram_followers = instagram_followers.text
+    print(final_instagram_name, 'has', final_instagram_followers, 'on Twitter')
+    driver.quit()
+
 
 def main():
-    twitter_followers_scraper()
+    #twitter_followers_scraper()
+    instagram_followers_scraper()
 
 
 
